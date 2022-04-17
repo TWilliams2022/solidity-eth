bb9f10d1e57d5c27d0b6d2cd277a0e9725a60e9a
Tyreik Williams <tyreik.mm.williams@gmail.com>
2022-04-16 22:40:36 -0400

    new

--- sol/Mycontract.sol
+++ sol/Mycontract.sol
@@ -1,7 +1,10 @@
 pragma solidity ^0.8.0;
 
-contract Mycontract {
-    function Mycontract(){
+//smart contract is code that gets executed on the blockchain\
+//people can access it, read and write data, execute any code inside of it
 
-    }
+contract Mycontract {
+    //write a *VALUE* to declare data type of variable we want to store
+    string value;
+    //this is where the data will get stored in the blockchain
 }
